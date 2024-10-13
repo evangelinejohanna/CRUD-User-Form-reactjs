122dab1c39ddc07aae3d96cf0b516a8ffc70181b
evangelinejohanna <evajo812@gmail.com>
2024-10-14 00:20:05 +0530

    commit

---
+++ App2.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import AddListForm from "./User-Form-CRUD/AddListForm";
+
+const App2 = () => {
+  
+  return (
+    <div>
+      <h1>User Form</h1>
+      <AddListForm />
+    </div>
+  );
+};
+
+export default App2;
+
+
